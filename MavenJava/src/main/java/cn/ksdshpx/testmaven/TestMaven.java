ad99db7fd7fdccfa0601326c166acf0a0c432e46
southeast_px <southeast_px@163.com>
2017-12-28 13:56:35 +0800

    IDEA创建基于maven的java项目！

---
+++ MavenJava/src/main/java/cn/ksdshpx/testmaven/TestMaven.java
@@ -0,0 +1,13 @@
+package cn.ksdshpx.testmaven;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2017/12/28
+ * Time: 13:53
+ */
+public class TestMaven {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven");
+    }
+}
